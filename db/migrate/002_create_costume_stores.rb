46e29fdbdaf90c3d6aedaef4787f72df7e49ac18
Alberto <ajsaenz1091@gmail.com>
2021-07-13 22:34:44 -0400

    Completed Assignment

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,9 +7,11 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
         t.string :name
         t.string :location
         t.integer :costume_inventory
-        t.integer :number_of_employees
-        t.boolean :family_friendly
-        t.time :opening_time
-        t.time :closing_time
+        t.integer :num_of_employees
+        t.boolean :still_in_business
+        t.datetime :opening_time
+        t.datetime :closing_time
+
+        t.timestamps null:false
     end
 end
